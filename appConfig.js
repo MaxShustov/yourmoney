8cb39fe6620f9f2657730e38cb2f31930ae08e1f
Maksym Shustov <maksym_shustov@epam.com>
2017-06-11 03:01:07 +0300

    Add config

---
+++ appConfig.js
@@ -0,0 +1,5 @@
+var settings = {
+    connectionString: "mongodb://yourmoney-dev:06eoNQBtgbe5TwTsbYzv7tWJr0OeUDlZKMuSwGgxXk0MHtOAvoaRDdnYALy1yDXgxJPHnC6pg7g1PJoKgHM0jQ==@yourmoney-dev.documents.azure.com:10255/yourmoney?ssl=true"
+}
+
+module.exports = settings;

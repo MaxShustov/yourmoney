145964f35fe127f60af4cac46e2b3d472bd2aa3e
Maksym Shustov <maksym_shustov@epam.com>
2017-06-23 13:00:55 +0300

    Use alternative config

--- appConfig.js
+++ appConfig.js
@@ -1,5 +1,6 @@
 var settings = {
-    connectionString: "mongodb://yourmoney-dev:06eoNQBtgbe5TwTsbYzv7tWJr0OeUDlZKMuSwGgxXk0MHtOAvoaRDdnYALy1yDXgxJPHnC6pg7g1PJoKgHM0jQ==@yourmoney-dev.documents.azure.com:10255/yourmoney?ssl=true"
+    //connectionString: "mongodb://yourmoney-dev:06eoNQBtgbe5TwTsbYzv7tWJr0OeUDlZKMuSwGgxXk0MHtOAvoaRDdnYALy1yDXgxJPHnC6pg7g1PJoKgHM0jQ==@yourmoney-dev.documents.azure.com:10255/yourmoney?ssl=true"
+    connectionString: "mongodb://maksym_shustov:Password1!@ds064649.mlab.com:64649/yourmoney"
 }
 
 module.exports = settings;
